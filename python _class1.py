9381ac47579d73d0b2a29a847d249d534a58202e
Khushi Singh <itskhushi.singh4545@gmail.com>
2025-07-16 13:20:32 +0530

    Add files via upload

---
+++ python _class1.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#operators
+
+
+# In[ ]:
+
+
+1.Arithimetic Operator
+
+
+# In[10]:
+
+
+a=10
+b=20
+c=a+b
+print(c)
+
+
+# In[11]:
+
+
+d=a-b
+print(d)
+
+
+# In[12]:
+
+
+e=a*b
+print(e)
+
+
+# In[13]:
+
+
+s=a/b
+print(s)
+
+
+# In[ ]:
+
+
+2.Assignment Operator
+
+
+# In[17]:
+
+
+a=5
+a+=10
+print(a)
+
+
+# In[25]:
+
+
+a=+10
+print(a)
+
+
+# In[ ]:
+
+
+3.comparison operator
+
+
+# In[ ]:
+
+
+<,>,<=,>=,==,!=
+
+
+# In[19]:
+
+
+a=10
+b=15
+c=a<b
+print(c)
+
+
+# In[20]:
+
+
+c=a>b
+print(c)
+
+
+# In[21]:
+
+
+c=a<=b
+print(c)
+
+
+# In[26]:
+
+
+c=a>=b
+print(c)
+
+
+# In[29]:
+
+
+a=10
+b=10
+c=a==b
+print(c)
+
+
+# In[ ]:
+
+
+4.Logical Operator
+
+
+# In[ ]:
+
+
+AND,OR,NOT
+
+
+# In[32]:
+
+
+a=10
+b=20
+print(a>b and a<b)
+print(a>b or a<b)
+print(not a>b) 
+
+
+# In[ ]:
+
+
+5.membership operators
+
+
+# In[ ]:
+
+
+in,not in
+
+
+# In[33]:
+
+
+a=2,4,5,6
+print(5 in a)
+
+
+# In[34]:
+
+
+print(6 not in a)
+
+
+# In[35]:
+
+
+print(8 not in a)
+
+
+# In[ ]:
+
+
+6.Bitwise logical operator
+
+
+# In[ ]:
+
+
+&, |,!
+
+
+# In[46]:
+
+
+a=4
+b=3
+print(a & b)
+print(a | b)
+print(~ b)
+
+
+# In[ ]:
+
+
+7.Identity operator
+
+
+# In[ ]:
+
+
+is, is not
+
+
+# In[2]:
+
+
+a=[1, 2, 3]
+b=a
+c=[1,2,3]
+print(a is b)
+
+
+# In[3]:
+
+
+print (a is c)
+
+
+# In[4]:
+
+
+print(a is not c)
+
+
+# In[ ]:
+
+
+
+
